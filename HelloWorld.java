d15e4dee8f156fb819dff264d0e651cb676ecc98
mygithubq <670236129@qq.com>
2015-04-09 21:37:09 +0800

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package test;
+/**
+ * 第一题 helloWord
+ * 
+ * @author 王倩倩
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("hello,world!");
+	}
+}
